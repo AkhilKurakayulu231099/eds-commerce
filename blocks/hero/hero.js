04bbaa2b589dd4116c3f6e746d653d05b63a2e92
AkhilKurakayulu231099 <akhil.kurakayulu@infosys.com>
2025-02-11 09:44:46 +0530

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,7 +1,1 @@
-export default async function decorate(block) {
-//Getting main Div container
-var mainDivOnne = document.querySelector('.home');
-//adding classNames to main Div
-var containerNewwDiv = document.createElement('div');
-containerNewwDiv.setAttribute('class','lyt-container');
-mainDivOnne.appendChild(containerNewwDiv);
+
