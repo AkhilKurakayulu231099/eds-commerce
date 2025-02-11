60c8b3d08e12684f9b86c70da57544c7c12b758a
AkhilKurakayulu231099 <akhil.kurakayulu@infosys.com>
2025-02-11 10:08:29 +0530

    Update home.js

--- blocks/home/home.js
+++ blocks/home/home.js
@@ -1,6 +1,6 @@
 export default async function decorate(block) {
 var videoDiv = document.querySelector('.home > div > div ');
   videoDiv.classList.add('videoclass');
-var buttonDiv = document.querySelector('.home > div > div > div ');
+var buttonDiv = document.querySelector('.home > div > div:nth-child(2) ');
   buttonDiv.classList.add('buttonclass');
 }
