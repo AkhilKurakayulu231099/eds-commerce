a848a2783d8ec6be6b99f5ce29aa93177d270fad
Arti-EDS <arti.khutwad@infosys.com>
2025-02-11 16:09:34 +0530

    Update card.js

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -2,9 +2,9 @@ export default async function decorate(block) {
     var cardDiv = document.querySelector('.card > div');
     cardDiv.classList.add('card-section');
 
-    var cardBlockOne = document.querySelector('.card > div > div:first-child');
-    cardBlockOne.classList.add('card-block-blue');
-
     var cardBlockTwo = document.querySelector('.card > div > div');
     cardBlockTwo.classList.add('card-block');
+  
+    var cardBlockOne = document.querySelector('.card > div > div:first-child');
+    cardBlockOne.classList.add('card-block-blue');
 }
