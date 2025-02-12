291cdcc281b1eab0db2ac1f5343e0ed5664a3952
Mariyal-EDS <mariyal.a@infosys.com>
2025-02-12 16:27:40 +0530

    shoptwo vlock changes

--- blocks/shoptwo/shoptwo.js
+++ blocks/shoptwo/shoptwo.js
@@ -1,5 +1,5 @@
 export default async function decorate(block) {
-    var buttonShoptwo = document.querySelector('div:nth-child(1) > div:nth-child(2) > p.button-container');
+    var buttonShoptwo = document.querySelector('.shoptwo > div:nth-child(2) > p.button-container');
     buttonShoptwo.classList.add('shoptwobutton');
     ;
 }
