2b8c923cdf76abc5258fca2cf1b8c7af2989adc9
Mariyal-EDS <mariyal.a@infosys.com>
2025-02-12 16:58:41 +0530

    css js changes

---
+++ blocks/shopnow/shopnow.js
@@ -0,0 +1,5 @@
+export default async function decorate(block) {
+    var buttonShopnow = document.querySelector('.shopnow > div:nth-child(1) > div:nth-child(2) > p.button-container');
+    buttonShopnow.classList.add('shopnowbutton');
+    
+}
