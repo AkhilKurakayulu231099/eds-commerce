df82031106fdc29609252573865251fe1184b473
Arti-EDS <arti.khutwad@infosys.com>
2025-02-13 12:26:16 +0530

    Update articletext.js

--- blocks/articletext/articletext.js
+++ blocks/articletext/articletext.js
@@ -1,4 +1,7 @@
-/*export default async function decorate(block) {
-    var articleDiv = document.querySelector('.articletext > div');
+export default async function decorate(block) {
+    var articleDiv = document.querySelector('.article > div');
     articleDiv.classList.add('article-section');
-}*/
+
+  var articleWhiteDiv = document.querySelector('.article-white');
+  articleWhiteDiv.classList.add('article');
+}
