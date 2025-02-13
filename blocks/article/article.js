2b2aeef15ef81d6b4734866c6ab3a0ee43d73923
Arti-EDS <arti.khutwad@infosys.com>
2025-02-13 15:10:42 +0530

    Update article.js

--- blocks/article/article.js
+++ blocks/article/article.js
@@ -1,18 +1,18 @@
 export default async function decorate(block) {
-    var articleWhiteDiv = document.querySelector('.article-white');
-    articleWhiteDiv.classList.add('article');
+    // var articleWhiteDiv = document.querySelector('.article-white');
+    // articleWhiteDiv.classList.add('article');
 
-    var articleOneDiv = document.querySelector('.article-one');
-    articleOneDiv.classList.add('article');
+    // var articleOneDiv = document.querySelector('.article-one');
+    // articleOneDiv.classList.add('article');
 
     var articleTwoDiv = document.querySelector('.article-two');
     articleTwoDiv.classList.add('article');
 
-    var articleWhite = document.querySelector('.article-white > div') ;
-    articleWhite.classList.add('article-section-white');
+    // var articleWhite = document.querySelector('.article-white > div') ;
+    // articleWhite.classList.add('article-section-white');
   
-    var articleDiv = document.querySelector('.article-one > div');
-    articleDiv.classList.add('article-section-one');
+    // var articleDiv = document.querySelector('.article-one > div');
+    // articleDiv.classList.add('article-section-one');
   
     var articleTwo = document.querySelector('.article-two > div');
     articleTwo.classList.add('article-section-two');
