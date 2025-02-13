802ab087463936e9b7d1d1c8ef86dda6c8177393
Mariyal-EDS <mariyal.a@infosys.com>
2025-02-13 13:21:24 +0530

    css js changes

--- blocks/shoptwo/shoptwo.js
+++ blocks/shoptwo/shoptwo.js
@@ -1,5 +1,8 @@
 export default async function decorate(block) {
     var buttonShoptwo = document.querySelector('.shoptwo > div:nth-child(1) > div:nth-child(2) > p.button-container');
     buttonShoptwo.classList.add('shoptwobutton');
+
+    var imgShoptwo = document.querySelector('.shoptwo > div:nth-child(2) > div:nth-child(1)');
+    imgShoptwo.classList.add('imgShoptwo');
     
 }
