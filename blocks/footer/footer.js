0c4e79fc30165f8e42daad7e6fdd8629c8da461a
AkhilKurakayulu231099 <akhil.kurakayulu@infosys.com>
2025-02-14 12:44:43 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,19 +1,4 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
-
-/**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
 export default async function decorate(block) {
-  // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  const fragment = await loadFragment(footerPath);
-  
-  block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
   var contactDiv = document.querySelector('.contact-info > div');
   contactDiv.classList.add('contact-info-section');
 
@@ -22,5 +7,4 @@ export default async function decorate(block) {
 
   var emailDiv = document.querySelector('.contact-info > div:nth-child(3)');
   emailDiv.classList.add('email-info');
-  block.append(footer);
 }
