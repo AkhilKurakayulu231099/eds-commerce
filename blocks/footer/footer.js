aa3cab9d3243dce8d7e9860886763b3da571f7ea
Arti Khutwad <arti.khutwad@ad.infosys.com>
2025-02-14 12:25:49 +0530

    added footer css and js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,20 +1,10 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
-
-/**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
 export default async function decorate(block) {
-  // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  const fragment = await loadFragment(footerPath);
+  var contactDiv = document.querySelector('.contact-info > div');
+  contactDiv.classList.add('contact-info-section');
 
-  // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  var phoneDiv = document.querySelector('.contact-info > div:nth-child(2)');
+  phoneDiv.classList.add('phone-info');
 
-  block.append(footer);
+  var emailDiv = document.querySelector('.contact-info > div:nth-child(3)');
+  emailDiv.classList.add('email-info');
 }
