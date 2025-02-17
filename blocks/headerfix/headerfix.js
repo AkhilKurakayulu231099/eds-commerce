758e7fc0f1996998a77cf57626cce6559b115339
Mariyal-EDS <mariyal.a@infosys.com>
2025-02-17 12:19:26 +0530

    headerfix js

--- blocks/headerfix/headerfix.js
+++ blocks/headerfix/headerfix.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
     const divContent = document.querySelector('.headerfix-wrapper');
     const targetElement = document.querySelector('.nav-wrapper');
-    if (divContent && targetElement) {
-        targetElement.insertAdjacentHTML('beforeend', divContent.innerHTML);
-      }
+    // if (divContent && targetElement) {
+    //     targetElement.insertAdjacentHTML('beforeend', divContent.innerHTML);
+    //   }
 }
