0bc0d4b42d38feec70fbf66b9e06cb07e751d793
Mariyal-EDS <mariyal.a@infosys.com>
2025-02-18 11:27:36 +0530

    Update product-list-page.js

--- blocks/product-list-page/product-list-page.js
+++ blocks/product-list-page/product-list-page.js
@@ -53,6 +53,7 @@ export default async function decorate(block) {
 
 // remove class in product list
 //$('.ds-sdk-product-image-carousel').removeClass('max-w-[200px]');
+  console.log("sdssssss");
 
   if (type !== 'search') {
     storeDetails.config.categoryName = document.querySelector('.default-content-wrapper > h1')?.innerText;
