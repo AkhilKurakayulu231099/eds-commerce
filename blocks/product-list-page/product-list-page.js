999ccd490d23465689530717edfaa5b105e122b5
Mariyal-EDS <mariyal.a@infosys.com>
2025-02-18 12:37:31 +0530

    Update product-list-page.js

--- blocks/product-list-page/product-list-page.js
+++ blocks/product-list-page/product-list-page.js
@@ -51,15 +51,7 @@ export default async function decorate(block) {
     },
   };
 
-// remove class in product list
-  var productimg = document.querySelector('.ds-sdk-product-image-carousel .flex-nowrap div');
-  console.log("sdssssss",productimg);
-  //productimg.classList.remove('max-w-[200px]');
-
-  
-  
-
-  if (type !== 'search') {
+if (type !== 'search') {
     storeDetails.config.categoryName = document.querySelector('.default-content-wrapper > h1')?.innerText;
     storeDetails.config.currentCategoryId = category;
     storeDetails.config.currentCategoryUrlPath = urlpath;
@@ -76,6 +68,9 @@ export default async function decorate(block) {
       }
     }, 200);
   });
-
+// remove class in product list
+  var productimg = document.querySelector('.ds-sdk-product-image-carousel .flex-nowrap div');
+  console.log("sdssssss",productimg);
+  //productimg.classList.remove('max-w-[200px]');
   return window.LiveSearchPLP({ storeDetails, root: block });
 }
